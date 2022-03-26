3e4ac1840f70f36bf0a4fee3f6a3d943ab37303e
Markus Lind <me@vskut.ru>
2022-03-26 20:34:01 +0300

    upd

---
+++ src/view/recordHistory.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="history">
+    {{ history.date }} - {{ history.info.main }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['history'],
+  data () {
+    return {
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.history {
+  border-radius: 5px;
+  padding: 5px 0;
+  margin-top: 5px;
+  &:nth-child(odd) {
+    background-color: rgba(220, 220, 220, 0.5);
+  }
+}
+</style>
