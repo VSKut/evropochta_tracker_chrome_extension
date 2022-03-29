b5f76900d6186a2e219b872ab55823ccc9cbc416
Markus Lind <me@vskut.ru>
2022-03-29 23:04:21 +0300

    upd

--- src/view/popup.vue
+++ src/view/popup.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="main_app">
     <h1>Ваши номера:</h1>
+    <button class="add">+</button>
     <div class="records_list">
+      <div v-if="records.length === 0" class="empty">Трэк номера отсутствуют.</div>
       <record v-for="(record, index) in records" :record="record" :index="index" :key="record.number"></record>
     </div>
   </div>
@@ -26,6 +28,11 @@ export default {
 }
 
 </script>
+<style>
+body {
+  margin: 0 8px;
+}
+</style>
 
 <style lang="scss" scoped>
 .main_app {
@@ -38,6 +45,29 @@ export default {
   width: 400px;
   max-height: 640px;
   overflow-y: scroll;
+  position: relative;
+
+  .add {
+    position: absolute;
+    right: 0;
+    top: 0;
+    width: 35px;
+    height: 35px;
+    background-color: rgba(107, 213, 179, 0.4);
+    border: solid 2px #5fbe9f;
+    border-top: 0;
+    color: #168566;
+    font-weight: 800;
+    border-radius: 0 0 5px 5px;
+    cursor: pointer;
+
+    &:hover {
+      background-color: rgba(107, 213, 179, 0.6);
+      border: solid 2px rgba(95, 190, 159, 0.74);
+      border-top: 0;
+      color: #0f5b46;
+    }
+  }
 
   h1 {
     margin: 5px 0 10px 0;
@@ -47,6 +77,13 @@ export default {
   }
 
   .records_list {
+    .empty {
+      text-align: center;
+      font-size: 12px;
+      font-weight: 600;
+      color: rgba(0, 0, 0, 0.4);
+    }
+
     &>div {
       padding-bottom: 8px;
 
